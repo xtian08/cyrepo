394cbc29f25602b5189a5406e6a99056b2b063ea
xtian08 <chriskint@yahoo.com>
2021-04-04 19:33:01 +0400

    Add files via upload

---
+++ A1Cloud.sh
@@ -0,0 +1,29 @@
+﻿#!/bin/bash
+
+Created by Chris Mariano
+
+#Switch to the /tmp directory
+cd /tmp
+
+#Download the Trend installer
+curl -O -k https://y5vpnu.manage.trendmicro.com:443/officescan/console/html/TMSM_HTML/ActiveUpdate/ClientInstall/tmsminstall.zip
+#curl -O -k https://y5vpnu.manage.trendmicro.com:443/officescan/console/html/TMSM_HTML/ActiveUpdate/ClientInstall/tmsmuninstall.zip
+curl -O -k https://raw.githubusercontent.com/xtian08/cyrepo/master/tmsmuninstall.zip
+
+#Unzip the installer
+unzip tmsminstall.zip
+unzip tmsmuninstall.zip
+
+#Install the Trend Software
+#/tmp/TMUninstallLauncher.app/Contents/MacOS/TMUninstallLauncher –-uninstall
+installer -pkg /tmp/tmsmuninstall/tmsmuninstall.pkg -target /
+installer -pkg /tmp/tmsminstall/tmsminstall.pkg -target /
+
+#Clean up the folder
+rm tmsminstall.zip
+rm tmsmuninstall.zip
+rm -rf /tmp/TMUninstallLauncher.app
+rm -rf /tmp/tmsminstall
+rm -rf /tmp/tmsmuninstall
+
+exit 0

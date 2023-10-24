c7ee0fcf63a1d0c0eb3d990e5aa2c60706157327
xtian08 <chriskint@yahoo.com>
2023-10-24 19:58:09 +0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,29 +1,45 @@
-# Define the URL for the Google Chrome release information
-$chromeReleaseURL = "https://omahaproxy.appspot.com/all.json"
 
-# Get the latest Chrome release information
-$chromeReleases = Invoke-RestMethod -Uri $chromeReleaseURL
+#Get Installed Version
+$ChromeRegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe'
 
-# Find the latest stable version of 64-bit Chrome
-$latestStableVersion = ($chromeReleases | Where-Object { $_.os == "win" -and $_.arch == "x64" -and $_.channel == "stable" } | Sort-Object -Property version -Descending)[0].version
-
-# Get the currently installed Chrome version
-$installedVersion = (Get-ItemProperty "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Google Chrome").DisplayVersion
-Write-host $installedVersion
-
-# Check if the installed version matches the latest version
-if ($installedVersion -ne $latestStableVersion) {
-    # Define the URL for the Google Chrome MSI installer
-    $chromeMsiURL = "https://dl.google.com/chrome/install/GoogleChromeStandaloneEnterprise64.msi"
-
-    # Download and install the latest MSI
-    $tempDir = [System.IO.Path]::GetTempPath()
-    $msiPath = Join-Path -Path $tempDir -ChildPath "GoogleChromeStandaloneEnterprise64.msi"
-    Invoke-WebRequest -Uri $chromeMsiURL -OutFile $msiPath
-    Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $msiPath /qn" -Wait
-
-    # Clean up the downloaded MSI
-    Remove-Item -Path $msiPath -Force
+if (Test-Path $ChromeRegistryPath) {
+    $GCVersionInfo = (Get-Item (Get-ItemProperty $ChromeRegistryPath).'(Default)').VersionInfo
+    $installedVersion = $GCVersionInfo.ProductVersion
+    echo "Installed is $installedVersion"
 } else {
-    Write-Host "Google Chrome is already up to date (version $latestStableVersion)."
+    echo "Chrome is not Installed"
 }
+
+#Get Latest Version
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$j = Invoke-WebRequest 'https://omahaproxy.appspot.com/all.json' | ConvertFrom-Json
+$stable = $j.versions | where-object { $_.channel -eq "stable" -and $_.os -eq "win64"}
+$latestStableVersion = $stable.version
+echo "Latest stable is $latestStableVersion"
+
+#Install Chrome
+$systemInfo = Get-WmiObject -Class Win32_ComputerSystem
+$architecture = $systemInfo.SystemType
+
+if ($architecture -like "*ARM*") {
+    Write-Host "ARM architecture is supported yet."
+} else {
+    # Check if the installed version matches the latest version
+    if ($installedVersion -ne $latestStableVersion) {
+        # Define the URL for the Google Chrome MSI installer
+        $chromeMsiURL = "https://dl.google.com/chrome/install/GoogleChromeStandaloneEnterprise64.msi"
+
+        # Download and install the latest MSI
+        $tempDir = [System.IO.Path]::GetTempPath()
+        $msiPath = Join-Path -Path $tempDir -ChildPath "GoogleChromeStandaloneEnterprise64.msi"
+        Invoke-WebRequest -Uri $chromeMsiURL -OutFile $msiPath
+        Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $msiPath /qn" -Wait
+        Write-Host "Installed version $latestStableVersion"
+
+        # Clean up the downloaded MSI
+        Remove-Item -Path $msiPath -Force
+    } else {
+        Write-Host "Google Chrome is already up to date (version $latestStableVersion)."
+    }
+}
+

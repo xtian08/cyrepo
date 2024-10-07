60dff9e7c27d01db75dc4f8aae53ec9edd2160ef
xtian08 <chriskint@yahoo.com>
2024-10-07 10:35:58 +0400

    Update ADMX.ScreenSaver.sh

--- ADMX.ScreenSaver.sh
+++ ADMX.ScreenSaver.sh
@@ -2,7 +2,7 @@
 # Created by Chris Mariano GIT:xtian08
 
 #Variables
-SSVersion="SSaverVer-04102024-02"
+SSVersion="SSaverVer-04102024-03"
 photoloc="/Users/Shared/NYUAD"
 repo="NYUAD-IT/nyuad"
 folder="SSaver"
@@ -14,31 +14,14 @@ scr_paths=(
     "/System/Library/ExtensionKit/Extensions/iLifeSlideshows.appex"
 )
 
-# Check if SSaver is current
-if [ -e "/Users/Shared/$SSVersion" ]; then
-    echo "SSaver is current."
-    sudo bash -c "$(curl -fsSL $macOS14SS)"
-    exit 0
+# Check if the folder exists before attempting to delete it
+if [ -d "$photoloc" ]; then
+    sudo rm -rf "$photoloc"
+    echo "Folder $photoloc deleted."
 else
-    # Delete files in /tmp starting with "ver"
-    echo "Deleting files old SSaverVer-* files..."
-    sudo rm -f /Users/Shared/SSaverVer*
-
-    # Check if the folder exists before attempting to delete it
-    if [ -d "$photoloc" ]; then
-        sudo rm -rf "$photoloc"
-        echo "Folder $photoloc deleted."
-    else
-        echo "Folder $photoloc does not exist."
-        mkdir -p "$photoloc"
-    fi
+    echo "Folder $photoloc does not exist."
+    mkdir -p "$photoloc"
 fi
-
-# Fetch and download files
-curl -s "https://api.github.com/repos/$repo/contents/$folder" | \
-grep '"name":' | cut -d '"' -f 4 | while read -r file; do
-  sudo curl -L -o "$photoloc/$file" "https://raw.githubusercontent.com/$repo/master/$folder/$file"
-done
 
 # Function to apply screensaver settings
 apply_screensaver_settings() {
@@ -49,7 +32,7 @@ apply_screensaver_settings() {
     sudo -u "$Cuser" defaults -currentHost write com.apple.ScreenSaverPhotoChooser ShufflesPhotos -bool false
     sudo -u "$Cuser" defaults -currentHost write com.apple.ScreenSaver.iLifeSlideShows styleKey -string "Classic"
     sudo -u "$Cuser" defaults -currentHost write com.apple.screensaver CleanExit -bool true
-    sudo -u "$Cuser" defaults -currentHost write com.apple.screensaver idleTime -int 600
+    #sudo -u "$Cuser" defaults -currentHost write com.apple.screensaver idleTime -int 600
     sudo -u "$Cuser" defaults -currentHost write com.apple.screensaver showClock -bool true
     sudo -u "$Cuser" defaults -currentHost write com.apple.screensaver tokenRemovalAction -int 0
     sudo -u "$Cuser" defaults -currentHost write com.apple.screensaver moduleDict -dict-add moduleName "iLifeSlideshows"
